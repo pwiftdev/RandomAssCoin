7a1cabf9a9eddb2fbf94bdf22f734f3561efd78e
bakardii01 <mickovicbalsa@gmail.com>
2026-08-03 23:06:57 +0200

    asdad
    asda

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const CONTRACT_ADDRESS = 'PASTE_CONTRACT_ADDRESS_HERE';
+export const CONTRACT_ADDRESS = 'FAKUXs9YUJTCEKGpAPdRVACaZaXqGTwxn86cXCi6pump';
 
 export const PUMP_FUN_URL = `https://pump.fun/coin/${encodeURIComponent(CONTRACT_ADDRESS)}`;
